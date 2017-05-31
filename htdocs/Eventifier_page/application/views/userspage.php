0336e08faf14ce3f0f02f121f193cfa622a12a4e
David Kalanda <David Kalanda>
2017-05-31 09:07:51 -0500

    Fixed video and nave icons

--- htdocs/Eventifier_page/application/views/userspage.php
+++ htdocs/Eventifier_page/application/views/userspage.php
@@ -1,13 +1,7 @@
 
 
     <!-- Header Carousel -->
-    <header id="myCarousel" class="carousel slide">
-        <!-- Indicators -->
-        <!-- <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol> -->
+    <!-- <header id="myCarousel" class="carousel slide"> -->
 
         <!-- Wrapper for slides -->
         <!-- <div class="carousel-inner">
@@ -20,29 +14,9 @@
                       <a href="<?= base_url('signup') ?>";>Sign up</a>
                     </button>
                 </div>
-            </div> -->
-            <!-- <div class="item">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 2</h2>
-                </div>
             </div>
-            <div class="item">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 3</h2>
-                </div>
-            </div> -->
         </div>
-
-        <!-- Controls -->
-        <!-- <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="icon-prev"></span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="icon-next"></span>
-        </a> -->
-    </header>
+    </header>  -->
 
     <!-- Page Content -->
     <div class="container">
